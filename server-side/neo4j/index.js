292c5169046d8365a9ea2fe153b6f5e94ee96258
Oever González <notengobattery@gmail.com>
2019-05-12 13:14:12 -0600

    [PRO2] Agregar las dependencias para Node.jsy

--- server-side/neo4j/index.js
+++ server-side/neo4j/index.js
@@ -1,26 +1,40 @@
-var express = require('express');
-var app = express();
-var port = 10000;
-const router = express.Router();
+/* Todos los paquetes */
+const express = require('express'); /* Pedir Express.js a Node.js */
+const util = require('util'); /* Utilidades para objetos de JavaScript */
+const neode = require('neode');
 
-app.use('/api/v1', router);
+/* Variables y constantes del programa */
+var app = express(); /* Crear una aplicación de Express.js */
+var port = 10000; /* Un puerto no privilegiado para alojar el servidor */
+const router = express.Router(); /* El router de la aplicación */
+var root_endpoint = '/api/v1'; /* El endpoint raíz de la app será este */
+
+/* Define el puerto y el endpoint raíz de la app */
+app.use(root_endpoint, router);
 app.listen(port);
 
-console.log('Listening on port ' + port);
+console.log('Servidor iniciado y escuchando en el puerto ' + port);
 
-router.get('/', function(req, res) {
-    res.json({ message: 'API is Online!' });
+router.get('/', function (req, res) {
+    res.json({
+        message: root_endpoint + ' endpoint raíz en línea.'
+    });
 });
 
-router.use(function(req, res, next) {
-   console.log('Weve got something.');
-   next() //calls next middleware in the application.
+router.use(function (req, res, next) {
+    console.log('Solicitud recibida, URL de petición: ' +
+        util.inspect(req.originalUrl) + ' query: ' + util.inspect(req.query));
+    next();
 });
 
 router.route('/numbers/:number').get((req, res) => {
-     res.json({result: req.params.number + 1})
+    res.json({
+        result: req.params.number + 1
+    })
 });
 
 router.route('/letters/:letter').get((req, res) => {
-     res.json({result: req.params.letter.toUpperCase()})
+    res.json({
+        result: req.params.letter.toUpperCase()
+    })
 });
